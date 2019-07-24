3437138000fa35d338f07d0b74509f26831d18b2
Steve Neale <steveneale3000@gmail.com>
2019-07-24 22:52:51 +0100

    Fix formatting
    Fixed formatting of relation_extractor.py and src/io/utils.py to
    ensure compliance with PEP8 guidelines.

--- src/io/utils.py
+++ src/io/utils.py
@@ -15,14 +15,14 @@ import os
 def load_from_file(file_path, lines=True, stripped=False):
 
     with open(file_path, "r", encoding="utf-8") as loaded_file:
-        if lines == True:
-            return [line.strip() for line in loaded_file.read().splitlines()] if stripped == True else loaded_file.read().splitlines() 
+        if lines is True:
+            return [line.strip() for line in loaded_file.read().splitlines()] \
+                    if stripped is True else loaded_file.read().splitlines()
         else:
             return loaded_file.read()
 
 
 def create_directory(directory_path):
-
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
     return directory_path
